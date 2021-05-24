9fbc613c0ac8c2c9e26f67ceb2cb8b514b05b6dc
kadmosb1 <k.j.vanderlelij@hhs.nl>
2021-05-24 11:14:04 +0200

    Initial commit

---
+++ src/main/java/nl/hhs/Lane.java
@@ -0,0 +1,27 @@
+package nl.hhs;
+
+public class Lane {
+
+	private int number;
+	private boolean occupied;
+
+	public Lane (int number) {
+		this.number = number;
+		this.occupied = false;
+	}
+
+	public int getNumber ()
+	{
+		return this.number;
+	}
+
+	public boolean isOccupied ()
+	{
+		return this.occupied;
+	}
+
+	public void setOccupied (boolean occupied)
+	{
+		this.occupied = occupied;
+	}
+}
